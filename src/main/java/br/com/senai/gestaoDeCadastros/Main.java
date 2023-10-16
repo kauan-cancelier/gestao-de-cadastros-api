c072b057fafc4d3c934c025cec86784874259e14
Kauan Mello Cancelier <kauanmellocancelier2005@gmail.com>
2023-10-16 17:00:49 -0300

    Iniciado repositório do projeto.

---
+++ src/main/java/br/com/senai/gestaoDeCadastros/Main.java
@@ -0,0 +1,28 @@
+package br.com.senai.gestaoDeCadastros;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
+@SpringBootApplication
+public class Main {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+	
+	@Bean
+	public Hibernate5JakartaModule jsonHibernate5Module() {
+		return new Hibernate5JakartaModule();
+	}
+	 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+		};
+	}
+}
