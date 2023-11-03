2071645cefdb9285f4554c01aa8a1a533c6dc3e0
DESKTOP-A0EDSUE\kauan <kauanmellocancelier2005@gmail.com>
2023-11-02 22:09:19 -0300

    Criado controller helper.

---
+++ src/main/java/br/com/senai/gestaoDeCadastros/util/ControllerHelper.java
@@ -0,0 +1,19 @@
+package br.com.senai.gestaoDeCadastros.util;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+public class ControllerHelper {
+	
+	private ControllerHelper() {}
+	
+	public static Pageable paginar(Optional<Integer> pagina) {
+		if (pagina.isPresent()) {
+			return PageRequest.of(pagina.get(), 15);
+		} else {
+			return PageRequest.of(0, 15);
+		}
+	}
+}
