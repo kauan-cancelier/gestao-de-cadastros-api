f380289bf84d10e7dd926e32f396447fe3734abf
KAUAN MELLO CANCELIER <kauan_cancelier@estudante.sesisenai.org.br>
2023-11-08 22:24:13 -0300

    Corrigido validações ineficientes do DTO de usuários.

--- src/main/java/br/com/senai/gestaoDeCadastros/dto/UsuarioDto.java
+++ src/main/java/br/com/senai/gestaoDeCadastros/dto/UsuarioDto.java
@@ -3,18 +3,16 @@ package br.com.senai.gestaoDeCadastros.dto;
 import br.com.senai.gestaoDeCadastros.entity.enums.Role;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class UsuarioDto {
 	
-	@Positive
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@PositiveOrZero
+	@NotNull(message = "O id é obrigatório. ")
 	private Integer id;
 	
 	@NotBlank(message = "O email é obrigatório. ")
